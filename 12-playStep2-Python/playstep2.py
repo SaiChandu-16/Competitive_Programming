7684ca459cf679fdb97b81fdb7f719d964226f75
SaiChandu-16 <71496047+SaiChandu-16@users.noreply.github.com>
2021-07-13 09:50:54 +0530

    playstep done

--- 12-playStep2-Python/playstep2.py
+++ 12-playStep2-Python/playstep2.py
@@ -34,5 +34,29 @@
 # Hint: Also, remember to use % to get the one's digit, and use //= to get rid of the one's digit.
 
 def playstep2(hand, dice):
+	
 	# your code goes here
-	pass
+	hand = list(str(hand))
+	dice = list(str(dice))
+	if hand[0] == hand[1]:
+		hand[2] = dice[-1]
+		dice = dice[:-1]
+	elif hand[1] == hand[2]:
+		hand[0] = dice[-1]
+		dice = dice[:-1]
+	else:
+		if hand[0] < dice[-1]:
+			hand[1] = hand[0]
+			hand[0] = dice[-1]
+			hand[2] = dice[-2]
+			dice = dice[:-2]
+		else:
+			hand[1] = dice[-1]
+			hand[2] = dice[-2]
+			dice = dice[:-2]
+				
+    
+	hand.sort(reverse=True)
+	hand = int("".join(hand))
+	dice = int("".join(dice))
+	return (hand, dice)
