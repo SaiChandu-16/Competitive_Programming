c4821ed15d2a4c48897afdd84dd2b4bf6f324510
SaiChandu-16 <71496047+SaiChandu-16@users.noreply.github.com>
2021-07-21 11:58:37 +0530

    happy number

--- 11-ishappynumber-Python/ishappynumber.py
+++ 11-ishappynumber-Python/ishappynumber.py
@@ -19,4 +19,26 @@
 
 def ishappynumber(n):
 	# your code goes here
-	pass
+	if(n<0):
+		return False
+	elif(n==1):
+		return True
+	while (n>=10):
+		n=digitSumSquare(n)
+		if(n==1):
+			return True
+	return False
+		
+
+	
+
+def digitSumSquare(n):
+	b = 0
+	while(n>0):
+		a = n%10
+		b = b+(a*a)
+		n = n//10
+		
+	return b		
+
+
