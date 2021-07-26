6e9137de876d8ee7b464f3b69ca0d1676d43fa45
SaiChandu-16 <71496047+SaiChandu-16@users.noreply.github.com>
2021-07-26 20:40:39 +0530

    binary

--- 05-binarytreepractice-Python/binarytree.py
+++ 05-binarytreepractice-Python/binarytree.py
@@ -8,58 +8,37 @@ class BinaryTree(object):
     def __init__(self, root):
         self.root = Node(root)
 
-def search(self, find_val):
-    """
-    Return True if the find_val is in the tree and False otherwise.
-    """
-    # Your code goes here
-    
-    if(find_val == self.find_val):
-        return self
-    if(find_val < self.find_val):
-        return self.left.search(find_val) if self.left else None
-    if(find_val > self.find_val):
-        return self.right.search(find_val) if self.right else None
-        
+    def search(self, find_val):
+        """
+        Return True if the find_val is in the tree and False otherwise.
+        """
+        # Your code goes here
+        return self.preorder_search(self.root, find_val)
 
-def print_tree(tree):
-    """
-    Print out all tree nodes as they are visited in a pre-order traversal."""
-    # Your code goes here
-    # if(tree.left):
-    #     tree.left.print_tree().value
-    #     print(tree.left.value)
-    # print(tree.value)
-    # if(tree.right):
-    #     tree.right.print_tree().value
-    
-    # print_tree(tree.left)
-    # print(tree," sdkjgosdhg")
-    # print_tree(tree.right)
+    def print_tree(self):
+        """
+        Print out all tree nodes as they are visited in a pre-order traversal."""
+        # Your code goes here
+        self.preorder_print(self.root)
 
+    def preorder_search(self, start, find_val):
+        """
+        Helper method - use this to create a recursive search solution.
+        """
+        # Your code goes here
+        if start == None:
+            return False
+        if start.value == find_val:
+            return True
+        return self.preorder_search(start.left, find_val) or self.preorder_search(start.right, find_val)
 
-def preorder_search(self, start, find_val):
-    """
-    Helper method - use this to create a recursive search solution.
-    """
-    # Your code goes here
-    pass
-
-def preorder_print(self, start, traversal):
-    """
-    Helper method - use this to create a recursive print solution.
-    """
-    # Your code goes here
-    pass
-
-
-tree = BinaryTree(1)
-print("zvjs")
-tree.root.left = Node(2)
-print("dfnsdn")
-tree.root.right = Node(3)
-tree.root.left.left = Node(4)
-print("dlgjdog")
-tree.root.left.right = Node(5)
-print("agljfbsdjg")
-print_tree(tree)
+    def preorder_print(self, start, traversal):
+        """
+        Helper method - use this to create a recursive print solution.
+        """
+        # Your code goes here
+        if start is None:
+            return
+        print(start.value)
+        self.preorder_print(start.left)
+        self.preorder_print(start.right)
