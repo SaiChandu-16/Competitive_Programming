200add9550ea0d0da9ea778f732ae6b396ae0008
SaiChandu-16 <71496047+SaiChandu-16@users.noreply.github.com>
2021-08-06 22:00:45 +0530

    limited set

--- set.py
+++ set.py
@@ -6,7 +6,23 @@
 # Example:
 # 	limitedPowerSet(5, 7) => 
 # [ {}, {1}, {2}, {3}, {4}, {5}, {1, 2} ]
-
+import itertools
 def limitedPowerSet(n, k):
     # Your code goes here...
-    pass
+    a = set(())
+    lst = [{}]
+    for i in range(1, n+1):
+        a.add(i)
+    for i in range(1, len(a)+1):
+        x = list(map(set, itertools.combinations(a,i)))
+        for j in range(len(x)):
+            if len(lst) != k:
+                lst.append(x[j])
+            else:
+                return lst
+
+assert(limitedPowerSet(6, 12) == [ {}, {1}, {2}, {3}, {4}, {5}, {6}, {1, 2}, {1, 3}, {1, 4}, {1, 5}, {1, 6}])
+assert(limitedPowerSet(7, 8) == [ {}, {1}, {2}, {3}, {4}, {5}, {6}, {7} ])
+assert(limitedPowerSet(5, 7) == [ {}, {1}, {2}, {3}, {4}, {5}, {1, 2} ])
+
+print("All test cases passed...!")
